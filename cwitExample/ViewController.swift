8389f2e57948634039a8da575a3cee671300de96
Kim Arnett <kim.arnett@hpe.com>
2016-08-30 15:50:42 -0400

    Finish example application. Add images.

--- cwitExample/ViewController.swift
+++ cwitExample/ViewController.swift
@@ -9,17 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //Connections to storyboard
+    @IBOutlet var welcomeLabel: UILabel!
+    @IBOutlet var nameTextField: UITextField!
 
+    @IBOutlet var submitButton: UIButton!
+    
+    //variables
+    let segueIdentifer = "welcome"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Any setup code here before the view shows to the user
+        
+        //customize color for welcome label
+        self.welcomeLabel.textColor = UIColor.greenColor()
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // This function is called when the app is about to crash due ot Memory overload. Dismiss any resources that can be re-created here.
     }
-
-
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        //this method is called during execution of a segue, before the next view loads. you can pass data etc.
+        
+        //here we take the text from our textfield and pass it to the WelcomeViewController's name variable
+        if segue.identifier == self.segueIdentifer {
+            //make sure this is the segue we were expecting..
+            
+            let welcomeVC = segue.destinationViewController as? WelcomeViewController
+            welcomeVC!.name = self.nameTextField.text
+            
+        }
+    }
+    
 }
 
---
+++ cwitExample/WelcomeViewController.swift
@@ -0,0 +1,61 @@
+//
+//  WelcomeViewController.swift
+//  cwitExample
+//
+//  Created by Kim Arnett on 8/30/16.
+//  Copyright © 2016 HPE USPS Mobility. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class WelcomeViewController: UIViewController {
+    
+    @IBOutlet var welcomeLabel: UILabel!
+    
+    @IBOutlet var profileImage: UIImageView!
+    
+    var name: String?  //? Means this value can be nil
+    
+    let images: [String] = ["puppy", "horse", "zebra", "whale", "lion"]  //An array of image names (Strings). "let" means this is a constant and cannot change.
+    
+    let noImage = "noimage" //image name if no name was available.
+    
+    override func viewDidLoad() {
+        
+        if name != nil && name != "" {
+            self.welcomeLabel.text = "Hello, \(name!)" //use the ! to unwrap the optional value.
+            loadImage(true)
+        } else {
+            //no name available.
+            self.welcomeLabel.text = "Oops, You forgot a name!"
+            loadImage(false)
+        }
+    }
+    
+    func loadImage(hasName: Bool) {
+        
+        if hasName {
+            
+            //name is available, show random image from profile images array
+            
+            let randomIndex = Int(arc4random_uniform(UInt32(images.count)))
+            
+            self.profileImage.image = UIImage(named: images[randomIndex])
+            
+        } else {
+            self.profileImage.image = UIImage(named: noImage)
+        }
+    }
+    
+    @IBAction func dismissBtnPressed(sender: AnyObject) {
+        
+        self.dismissViewControllerAnimated(true, completion: {
+            //if you need to do anything when view finishes dismissing, do it here.
+        })
+    }
+    
+    
+    
+}
